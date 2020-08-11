d302a16dc585ca9300727c377329e2bf9539c9c9
shivam7374 <shivamsingla491@gmail.com>
2020-08-11 08:57:09 +0530

    Navbar UI Done

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,57 @@ class App extends React.Component {
     const { posts } = this.props;
     return (
       <div>
+        <nav className="nav">
+          <div className="left-div">
+            <img
+              src="https://ninjasfiles.s3.amazonaws.com/0000000000003454.png"
+              alt="logo"
+            ></img>
+          </div>
+          <div className="search-container">
+            <img
+              className="search-icon"
+              src="https://image.flaticon.com/icons/svg/483/483356.svg"
+              alt="search-icon"
+            ></img>
+            <input placeholder="Search"></input>
+            <div className="search-results">
+              <ul>
+                <li className="search-results-row">
+                  <img
+                    src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+                    alt="user-dp"
+                  ></img>
+                  <span>BOSS</span>
+                </li>
+                <li className="search-results-row">
+                  <img
+                    src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+                    alt="user-dp"
+                  ></img>
+                  <span>BOSS</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="right-nav">
+            <div className="user">
+              <img
+                src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+                alt="user-dp"
+                id="user-dp"
+              ></img>
+              <span>Boss</span>
+            </div>
+            <div className="nav-links">
+              <ul>
+                <li>Login</li>
+                <li>Logout</li>
+                <li>Register</li>
+              </ul>
+            </div>
+          </div>
+        </nav>
         <PostList posts={posts}></PostList>
       </div>
     );
