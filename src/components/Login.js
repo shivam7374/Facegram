9dc035aa26df7f2d3a25102c30c39ce4849d3e21
shivam7374 <shivamsingla491@gmail.com>
2020-08-12 14:58:08 +0530

    Conveting login form from uncontrolled component to controlled component

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,13 +3,33 @@ import React, { Component } from "react";
 export default class Login extends Component {
   constructor(props) {
     super(props);
-    this.emailInputRef = React.createRef();
-    this.passwordInputRef = React.createRef();
+    // this.emailInputRef = React.createRef();
+    // this.passwordInputRef = React.createRef();
+    this.state = {
+      email: "",
+      password: "",
+    };
   }
+
+  handleEmailChange = (e) => {
+    // console.log(e.target.value);
+    this.setState({
+      email: e.target.value,
+    });
+  };
+
+  handlePasswordChange = (e) => {
+    // console.log(e.target.value);
+    this.setState({
+      password: e.target.value,
+    });
+  };
+
   handleFormSubmit = (e) => {
     e.preventDefault();
-    console.log("this.emailInputRef ", this.emailInputRef);
-    console.log("this.passwordInputRef ", this.passwordInputRef);
+    // console.log("this.emailInputRef ", this.emailInputRef);
+    // console.log("this.passwordInputRef ", this.passwordInputRef);
+    console.log("this.state ", this.state);
   };
   render() {
     return (
@@ -20,7 +40,10 @@ export default class Login extends Component {
             type="email"
             placeholder="Email"
             required
-            ref={this.emailInputRef}
+            // ref={this.emailInputRef}
+            onChange={this.handleEmailChange}
+            // value={this.state.email}
+            value={this.state.email}
           ></input>
         </div>
         <div className="field">
@@ -29,6 +52,8 @@ export default class Login extends Component {
             placeholder="Password"
             required
             ref={this.passwordInputRef}
+            onChange={this.handlePasswordChange}
+            // value={this.state.password}
           ></input>
         </div>
         <div className="field">
